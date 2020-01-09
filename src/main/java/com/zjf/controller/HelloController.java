7566f20b5f6d3c8697b8187e4b3e635df3dc1fd0
kfzx-zhoujf <150918120@qq.com>
2020-01-09 14:13:22 +0800

    init repo

---
+++ src/main/java/com/zjf/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.zjf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author zjf
+ * @create 2020/1/9-13:39
+ * @controller自动作为spring的bean处理
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
