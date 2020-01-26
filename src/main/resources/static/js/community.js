dea4fc53f97035b6dc882d7160b2e0ab17287204
kfzx-zhoujf <150918120@qq.com>
2020-01-26 10:01:24 +0800

    to response if needs login when comment

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -18,7 +18,16 @@ function post() {
             if (response.code == 200) {
                 $("#comment_section").hide();
             } else {
-                alert(response.message);
+                if (response.code == 2003) {
+                    //判断是不是登录异常,是则重新登录
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=2fa0027ee18392156348&redirect_uri=http://localhost:8887/callback&scope=user&state=1")
+                        window.localStorage.setItem("closable",true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
             console.log(response);
         },
